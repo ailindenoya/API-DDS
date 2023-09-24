666cf114039f56da5d525ede79c5355440bce7d9
LucasBatalla <lbatalla@frba.utn.edu.ar>
2023-09-24 12:17:54 -0300

    Clases para API y controllers

--- src/main/java/ClasesDominio/Entidad.java
+++ src/main/java/ClasesDominio/Entidad.java
@@ -7,6 +7,7 @@ public class Entidad {
   String nombre;
   List<Servicio> servicios;
 
+
   public Entidad(String nombre, List<Servicio> servicios) {
     this.nombre = nombre;
     this.servicios = servicios;
